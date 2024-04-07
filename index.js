9b9dc98bf5a9f97816deb35f0e742ea16f15240f
Jeronimo <familiadojeronimo@gmail.com>
2024-04-07 18:55:39 -0300

    Ferramentas pré selecionadas

---
+++ index.js
@@ -0,0 +1,5 @@
+/*
+    express         (para criação da api que sera consumida pelo front)
+    jsonwebtoken    (para adição de camanda de criptografia)
+    Sequelize       (para linkagem do back com o bd)
+*/ 
