69ec3a1136e491d3b8b4524dbff244c57d42973a
Jeronimo <familiadojeronimo@gmail.com>
2024-04-11 20:29:28 -0300

    2-criação-de-banco-de-dados

--- index.js
+++ index.js
@@ -2,4 +2,21 @@
     express         (para criação da api que sera consumida pelo front)
     jsonwebtoken    (para adição de camanda de criptografia)
     Sequelize       (para linkagem do back com o bd)
-*/ 
+*/
+
+/*
+1-definir qual porta utilizar 
+2-definir url da api
+3-regras de entrada
+*/
+
+let express =require('express');
+let api = express();
+
+
+api.get('/', function(req, res){
+    res.send('hellou world');
+
+});
+
+app.listen(3000)
