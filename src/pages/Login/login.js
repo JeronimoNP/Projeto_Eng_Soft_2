c7ba00d3e1179768fa096aab7938df7c9ea74fad
Letícia Borges <leticinhabb4@gmail.com>
2024-04-17 02:03:01 -0300

    Add js e ajustando css

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,21 +1,42 @@
-const login = document.getElementById('login')
-login.addEventListener('submit', async (e) => {
+const loginForms = document.getElementById('login')
+loginForms.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const email = document.getElementById('email').value
-    const passsword = document.getElementById('password').value
 
-    const response = await fetch('caminho pro back', {
-     method: 'POST',
-     headers: {
-        'content-type': 'application/json'
-     },   
-     body: JSON.stringify({email, passsword})
-    })
-    const data = await response.json()
+    if(localStorage.getItem('email') && localStorage.getItem('senha')) {
 
-    if(response.ok){[
-        window.location.href='#'
-    ]} else {
-        alert('Senha ou E-mail incorretos')
+        const storedEmail = localStorage.getItem('email');
+        const storedSenha = localStorage.getItem('senha');
+        if(email === storedEmail && senha === storedSenha) {
+            alert('Login bem-sucedido!');
+        } else {
+            alert('Email ou senha incorretos. Por favor, tente novamente.');
+            emailInput.classList.add('invalid');
+            senhaInput.classList.add('invalid');
+        }
+    } else {
+        alert('Nenhum dado de login encontrado. Por favor, cadastre-se.');
     }
-})
+    document.querySelectorAll('.campo-email, .campo-senha').forEach(input => {
+        input.addEventListener('input', function() {
+            this.classList.remove('invalid');
+        });
+    });
+});
+const mostrarSenhaIcon = document.getElementById('mostrar-senha');
+const senhaInput = document.getElementById('senha');
+
+mostrarSenhaIcon.addEventListener('click', function() {
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+        mostrarSenhaIcon.classList.remove('fa-eye-slash');
+        mostrarSenhaIcon.classList.add('fa-eye');
+    } else {
+        senhaInput.type = 'password';
+        mostrarSenhaIcon.classList.remove('fa-eye');
+        mostrarSenhaIcon.classList.add('fa-eye-slash');
+    }
+});
+
+
+
+
