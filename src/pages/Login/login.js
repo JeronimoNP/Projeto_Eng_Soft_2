fb30b016ff2c3c1867471f1fbe245e9c0c3f052e
Letícia Borges <leticinhabb4@gmail.com>
2024-04-18 15:10:04 -0300

    alterações js

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,22 +1,24 @@
-const login = document.getElementById('longin');
-
-login.addEventListener('submit', async (e) => {
+const loginForm = document.getElementById('login'); // Alterado de 'login' para 'loginForm'
+loginForm.addEventListener('submit', async (e) => {
     e.preventDefault();
+
+    // Obtenha os valores inseridos nos campos de email e senha
     const email = document.getElementById('email').value;
-    const passsword = document.getElementById('password').value;
+    const senha = document.getElementById('password').value;
 
-    const response = await fetch('caminho pro back', {
-     method: 'POST',
-     headers: {
-        'content-type': 'application/json'
-     },   
-     body: JSON.stringify({email, passsword})
-    })
-    const data = await response.json();
+    if(localStorage.getItem('email') && localStorage.getItem('senha')) {
+        // Obtenha os dados do localStorage
+        const storedEmail = localStorage.getItem('email');
+        const storedSenha = localStorage.getItem('senha');
 
-    if(response.ok){[
-        window.location.href='#'
-    ]} else {
-        alert('Senha ou E-mail incorretos');
+        // Verifique se os dados inseridos correspondem aos dados armazenados
+        if(email === storedEmail && senha === storedSenha) {
+            alert('Login bem-sucedido!');
+        } else {
+            alert('Email ou senha incorretos. Por favor, tente novamente.');
+        }
+    } else {
+        alert('Nenhum dado de login encontrado. Por favor, cadastre-se.');
     }
 });
+
